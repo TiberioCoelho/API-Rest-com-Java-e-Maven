30be0f2f4d7d9c469eabd34c69311f219ceb5eb1
Tiberio Coelho Inacio <tiberiocoelho@yahoo.com.br>
2021-05-25 12:39:49 -0300

    Delete Rest-API directory

--- Rest-API/src/main/java/com/github/tiberiocoelho/RestAPI/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.tiberiocoelho.RestAPI.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.github.tiberiocoelho.RestAPI.model.UsuarioModel;
-
-public interface UsuarioRepository extends CrudRepository<UsuarioModel, Integer> {
-
-}
